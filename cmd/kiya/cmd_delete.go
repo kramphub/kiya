8cc1637a281b6a2f16a42868752597b52cb63d80
Soheil Yahyaee <yahyaee.soheil@gmail.com>
2021-08-07 15:53:54 +0200

    Remove commented line

--- cmd/kiya/cmd_delete.go
+++ cmd/kiya/cmd_delete.go
@@ -11,7 +11,6 @@ import (
 // commandDelete deletes a stored key
 func commandDelete(ctx context.Context, b backend.Backend, target *backend.Profile, key string) {
 	if promptForYes(fmt.Sprintf("Are you sure to delete [%s] from [%s] (y/N)? ", key, target.Label)) {
-		//if err := kiya.DeleteSecret(kmsService, storageService, target, key); err != nil {
 		if err := b.Delete(ctx, target, key); err != nil {
 			fmt.Printf("failed to delete [%s] from [%s] because [%v]\n", key, target.Label, err)
 		} else {
