f2812ffca463ced22299800956840754d4cfe9a5
Arvind Mohabir <arvind@kramphub.com>
2022-08-15 11:53:34 +0200

    - Reverted to old location including fix

--- cmd/kiya/utils.go
+++ cmd/kiya/utils.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/kramphub/kiya/backend"
+	"golang.org/x/term"
 )
 
 func readFromStdIn() string {
@@ -46,4 +47,20 @@ func shouldPromptForPassword(b backend.Backend) bool {
 	default:
 		return false
 	}
+}
+
+func promptForPassword() []byte {
+	log.Print("[INFO]: Make sure you use a secure and strong master password.")
+
+	fmt.Println("Enter master password: ")
+	password, err := term.ReadPassword(int(os.Stdin.Fd()))
+
+	if err != nil {
+		log.Fatal("Error while reading password from standard in", err)
+	}
+
+	if len(password) == 0 {
+		log.Fatal("Password should have at least one character.")
+	}
+	return password
 }
