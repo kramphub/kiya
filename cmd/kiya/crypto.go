33a680b3e55d090a4580730102d289601e1b80d2
Denis <denis@ag5.com>
2022-10-21 14:26:23 +0200

    Added new utils

--- cmd/kiya/crypto.go
+++ cmd/kiya/crypto.go
@@ -4,12 +4,17 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/pem"
 	"fmt"
 	"io"
+	"os"
 )
 
+// generateSecret returns generated secret as base64 string
 func generateSecret() string {
 	key := make([]byte, 64)
 
@@ -21,9 +26,8 @@ func generateSecret() string {
 	return base64.URLEncoding.EncodeToString(key)
 }
 
-// generateKeyPair returns RSA key pair.
-func encrypt(data []byte, secret string) ([]byte, error) {
-	key := sha256.Sum256([]byte(secret))
+func encryptFile(data []byte, secret []byte) ([]byte, error) {
+	key := sha256.Sum256(secret)
 	block, err := aes.NewCipher(key[:])
 	if err != nil {
 		return nil, fmt.Errorf("create new Cipher failed, %s", err.Error())
@@ -41,8 +45,8 @@ func encrypt(data []byte, secret string) ([]byte, error) {
 	return ciphertext, nil
 }
 
-func decrypt(data []byte, secret string) ([]byte, error) {
-	key := sha256.Sum256([]byte(secret))
+func decryptFile(data []byte, secret []byte) ([]byte, error) {
+	key := sha256.Sum256(secret)
 	block, err := aes.NewCipher(key[:])
 	if err != nil {
 		return nil, fmt.Errorf("create new Cipher failed, %s", err.Error())
@@ -58,4 +62,79 @@ func decrypt(data []byte, secret string) ([]byte, error) {
 	stream.XORKeyStream(data, data)
 
 	return data, nil
+}
+
+func encryptSecret(secret string, publicKey *rsa.PublicKey) (encryptedSecret string, err error) {
+	buf, err := base64.URLEncoding.DecodeString(secret)
+	if err != nil {
+		return "", fmt.Errorf("decode base64 string failed, %w", err)
+	}
+
+	buf, err = rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, buf, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(buf), nil
+}
+
+func decryptSecret(secret string, privateKey *rsa.PrivateKey) ([]byte, error) {
+	secretBytes, err := base64.URLEncoding.DecodeString(secret)
+	if err != nil {
+		return nil, fmt.Errorf("decode base64 string failed, %w", err)
+	}
+	buf, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privateKey, secretBytes, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
+
+func generateKeyPair() (*rsa.PrivateKey, *rsa.PublicKey, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, nil, fmt.Errorf("generate RSA key pair failed, %w", err)
+	}
+	return privateKey, &privateKey.PublicKey, nil
+}
+
+// exportPublicKeyAsPEM returns public key as a string in PEM format
+func exportPublicKeyAsPEM(key *rsa.PublicKey) string {
+	pemStr := string(pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: x509.MarshalPKCS1PublicKey(key),
+		},
+	))
+	return pemStr
+}
+
+// exportPrivateKeyAsPEM returns private key as a string in PEM format
+func exportPrivateKeyAsPEM(key *rsa.PrivateKey) string {
+	pemStr := string(pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(key),
+		},
+	))
+	return pemStr
+
+}
+
+func exportPrivateKeyFromPEMString(pemStr []byte) *rsa.PrivateKey {
+	block, _ := pem.Decode(pemStr)
+	key, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
+	return key
+}
+
+func exportPublicKeyFromPEMString(pemStr []byte) *rsa.PublicKey {
+	block, _ := pem.Decode(pemStr)
+	key, _ := x509.ParsePKCS1PublicKey(block.Bytes)
+	return key
+}
+
+func saveKeyToFile(keyPem, filename string) error {
+	pemBytes := []byte(keyPem)
+	return os.WriteFile(filename, pemBytes, 0400)
 }
--- cmd/kiya/crypto_test.go
+++ cmd/kiya/crypto_test.go
@@ -1,8 +1,106 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"encoding/base64"
+	"log"
+	"testing"
+)
 
-func TestGenerateSecret(t *testing.T) {
+func TestEncryptSecret(t *testing.T) {
 	secret := generateSecret()
-	t.Logf("secret: %s", secret)
+
+	priv, pub, err := generateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	encryptedSecret, err := encryptSecret(secret, pub)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	decryptedSecret, err := decryptSecret(encryptedSecret, priv)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	originalSecret, err := base64.URLEncoding.DecodeString(secret)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(originalSecret, decryptedSecret) {
+		t.Fatalf("original secret and decrypted secret is not equla 😮")
+	}
+}
+
+func TestEncryptFile(t *testing.T) {
+	bak := Backup{
+		Secret: generateSecret(),
+		Data:   []byte{0x0, 0xf, 0xff, 0x54, 0x43},
+	}
+
+	buf, err := encryptFile(bak.Data, bak.SecretAsBytes())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf, err = decryptFile(buf, bak.SecretAsBytes())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(bak.Data) != len(buf) {
+		t.Fatalf("origin and decrypted data length is not equal")
+	}
+
+	if bak.Data[0] != buf[0] || bak.Data[1] != buf[1] || bak.Data[2] != buf[2] || bak.Data[3] != buf[3] || bak.Data[4] != buf[4] {
+		t.Fatalf("data in origin and decrypted is not equals")
+	}
+}
+
+func TestEncryptFileFull(t *testing.T) {
+	bak := Backup{
+		Secret: generateSecret(),
+		Data:   []byte("my super secret text"),
+	}
+
+	encryptedDataBuf, err := encryptFile(bak.Data, bak.SecretAsBytes())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	priv, pub, err := generateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	encryptedSecret, err := encryptSecret(bak.Secret, pub)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bakStr := (&Backup{Secret: encryptedSecret, Data: encryptedDataBuf}).String()
+
+	bak2 := Backup{}
+	bak2.FromString(bakStr)
+
+	if encryptedSecret != bak2.Secret {
+		log.Fatalf("secret is not equal")
+	}
+
+	decryptedSecret, err := decryptSecret(bak2.Secret, priv)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dBuf, err := decryptFile(bak2.Data, decryptedSecret)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(bak.Data, dBuf) {
+		t.Fatalf("origin and decrypted data is not equal")
+	}
 }
